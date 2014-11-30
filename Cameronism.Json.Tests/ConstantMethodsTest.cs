680a6548312f54a98ea2d189441c66f5bc6f70fb
Cameron Jordan <cjordan@cameronism.com>
2014-11-29 20:51:27 -0800

    Some internal support for constant methods

---
+++ Cameronism.Json.Tests/ConstantMethodsTest.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cameronism.Json.Tests
+{
+	public class ConstantMethodsTest
+	{
+		#region properties
+		string C2 { get { return null; } }
+		string C3 { get { return "a"; } }
+		int C4 { get { return 1; } }
+		int? C5 { get { return 1; } }
+		int? C6 { get { return null; } }
+		char C7 { get { return 'a'; } }
+		char? C8 { get { return 'a'; } }
+		char? C9 { get { return null; } }
+		DateTime C10 { get { return default(DateTime); } }
+		int C11 { get { return -1; } }
+		int C12 { get { return unchecked((int)0x80000000); } }
+		long C13 { get { return 0xffffffffff; } }
+		double C14 { get { return 0.5; } }
+		float C15 { get { return 0.5f; } }
+		sbyte C16 { get { return sbyte.MaxValue; } }
+		byte C17 { get { return byte.MaxValue; } }
+		short C18 { get { return short.MaxValue; } }
+		ushort C19 { get { return ushort.MaxValue; } }
+		uint C20 { get { return uint.MaxValue; } }
+		ulong C21 { get { return ulong.MaxValue; } }
+		long? C22 { get { return 0xffffffffff; } }
+		double? C23 { get { return 0.5; } }
+		float? C24 { get { return 0.5f; } }
+		sbyte? C25 { get { return sbyte.MaxValue; } }
+		byte? C26 { get { return byte.MaxValue; } }
+		short? C27 { get { return short.MaxValue; } }
+		ushort? C28 { get { return ushort.MaxValue; } }
+		uint? C29 { get { return uint.MaxValue; } }
+		ulong? C30 { get { return ulong.MaxValue; } }
+		Guid C31 { get { return default(Guid); } }
+		Guid? C32 { get { return default(Guid); } }
+		bool C33 { get { return true; } }
+		bool? C34 { get { return false; } }
+		bool? C35 { get { return null; } }
+		decimal C36 { get { return 1.1M; } }
+		decimal? C37 { get { return 1.1M; } }
+		decimal? C38 { get { return null; } }
+		DateTime D39 { get { return SomeDateTime(); } }
+		DateTime D40 { get { return new DateTime(2010, 1, 1, 1, 1, 1, DateTimeKind.Utc); } } // TODO
+		Guid D41 { get { return new Guid("3350c137-a6c7-4e3f-aa4d-182f068899c5"); } } // TODO
+		DelegateBuilderTest.B<int> D42 { get { return new DelegateBuilderTest.B<int>(); } }
+		#endregion
+		static DateTime SomeDateTime() { return new DateTime(2010, 1, 1, 1, 1, 1, DateTimeKind.Utc); }
+
+		static bool ToleratedFailure(Type type, string newtonsoft)
+		{
+			var underlying = Nullable.GetUnderlyingType(type) ?? type;
+
+			return newtonsoft != null &&
+				(underlying == typeof(float) || underlying == typeof(decimal));
+		}
+
+		[Fact]
+		public void ApproveConstants()
+		{
+			var methods = this.GetType().GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+			var sb = new StringBuilder();
+			int failedEqualCount = 0;
+
+			foreach (var mi in methods)
+			{
+				if (!mi.IsSpecialName || !mi.Name.StartsWith("get_")) continue;
+
+				var shouldBeConstant = mi.Name[4] == 'C';
+				var actual = mi.Invoke(this, null);
+				var nJSON = Newtonsoft.Json.JsonConvert.SerializeObject(actual);
+				var constant = ConstantMethods.TryGetJson(mi);
+
+				var passed = shouldBeConstant ?
+					String.Equals(nJSON, constant, StringComparison.Ordinal) :
+					constant == null;
+
+				sb.AppendLine("# " + mi.Name.Substring(4) + " " + SchemaTest.HumanName(mi.ReturnType));
+				sb.AppendLine();
+				sb.AppendLine("Expected constant: " + shouldBeConstant);
+				if (!passed)
+				{
+					if (ToleratedFailure(mi.ReturnType, nJSON))
+					{
+						sb.AppendLine("** SKIPPED ** - fix this");
+					}
+					else
+					{
+						failedEqualCount++;
+						sb.AppendLine("** FAILED **");
+					}
+				}
+
+				sb.AppendLine();
+				sb.AppendLine("## Newtonsoft");
+				Util.Hex.Dump(sb, Encoding.UTF8.GetBytes(nJSON));
+				sb.AppendLine();
+
+				sb.AppendLine();
+				sb.AppendLine("## Cameronism.Json");
+				if (constant != null)
+				{
+					Util.Hex.Dump(sb, Encoding.UTF8.GetBytes(constant));
+				}
+				else
+				{
+					sb.AppendLine();
+					sb.AppendLine("`null`");
+				}
+				sb.AppendLine();
+				sb.AppendLine();
+				sb.AppendLine();
+			}
+
+			ApprovalTests.Approvals.Verify(sb.ToString());
+			Assert.True(failedEqualCount == 0, "Look at the approval, " + failedEqualCount + " failed");
+		}
+	}
+}
---
+++ Cameronism.Json/ConstantMethods.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cameronism.Json
+{
+	internal static class ConstantMethods
+	{
+		static MethodInfo _Disassemble = typeof(ConstantMethods).GetMethod("Disassemble", BindingFlags.Static | BindingFlags.NonPublic);
+
+		public static string TryGetJson(MethodInfo mi)
+		{
+			return (string)_Disassemble.MakeGenericMethod(mi.DeclaringType, mi.ReturnType).Invoke(null, new object[] { mi });
+		}
+
+		static string Disassemble<T1, TReturn>(MethodInfo mi)
+		{
+			var del = (Func<T1, TReturn>)mi.CreateDelegate(typeof(Func<T1, TReturn>));
+			Exception ex;
+			var ops = TryDisassemble(del, out ex);
+			if (ops == null) return null;
+
+			return GetConstantJson(ops, mi);
+		}
+
+		public static Sigil.DisassembledOperations<T> TryDisassemble<T>(T func, out Exception ex) where T : class
+		{
+			try
+			{
+				ex = null;
+				return Sigil.Disassembler<T>.Disassemble(func);
+			}
+			catch (Exception e)
+			{
+				ex = e;
+				// probably sigil bug
+				return null;
+			}
+		}
+
+		const int MIN_INTEGRAL = (int)TypeCode.Boolean;
+		const int MAX_INTEGRAL = (int)TypeCode.UInt64;
+
+		static string GetConstantJson<T>(Sigil.DisassembledOperations<T> ops, MethodInfo mi)
+		{
+			string theValue = null;
+			var typeCode = (int)Type.GetTypeCode(Nullable.GetUnderlyingType(mi.ReturnType) ?? mi.ReturnType);
+			
+			foreach (var op in ops)
+			{
+				switch ((ushort)op.OpCode.Value)
+				{
+					case 0x00: // Nop
+					case 0x2a: // Ret
+					case 0x2b: // Br_S
+						continue;
+						
+					case 0x14: // Ldnull
+						theValue = "null";
+						continue;
+
+					case 0xfe15: // Initobj
+						theValue = GetDefaultValue(op.Parameters.FirstOrDefault() as Type);
+						continue;
+						
+					case 0x0a: // Stloc_0
+					case 0x06: // Ldloc_0
+					case 0x07: // Ldloc_1
+					case 0x12: // Ldloca_S
+					case 0x73: // Newobj
+					case 0x6a: // Conv_I8
+						continue;
+						
+					case 0x72: // Ldstr
+						if (mi.ReturnType == typeof(string))
+						{
+							theValue = GetEscapedString(op.Parameters.FirstOrDefault() as string);
+							continue;
+						}
+						else
+						{
+							return null;
+						}
+
+					case 0x15: // Ldc_I4_M1
+					case 0x16: // Ldc_I4_0
+					case 0x17: // Ldc_I4_1
+					case 0x18: // Ldc_I4_2
+					case 0x19: // Ldc_I4_3
+					case 0x1a: // Ldc_I4_4
+					case 0x1b: // Ldc_I4_5
+					case 0x1c: // Ldc_I4_6
+					case 0x1d: // Ldc_I4_7
+					case 0x1e: // Ldc_I4_8
+					case 0x1f: // Ldc_I4_S
+					case 0x20: // Ldc_I4
+						if (typeCode >= MIN_INTEGRAL && typeCode <= MAX_INTEGRAL)
+						{
+							theValue = GetInt32Value(op.Parameters.FirstOrDefault(), mi.ReturnType);
+							continue;
+						}
+						else
+						{
+							return null;
+						}
+						
+					case 0x21: // Ldc_I8
+						theValue = GetInt64Value(op.Parameters.FirstOrDefault(), mi.ReturnType);
+						continue;
+
+					case 0x22: // Ldc_R4
+					case 0x23: // Ldc_R8
+						theValue = GetDoubleValue(op.Parameters.FirstOrDefault(), mi.ReturnType);
+						continue;
+					
+					// assume non constant if we encounter anything else
+					default:
+						//op.Dump("bailing");
+						//op.OpCode.Value.ToString("x").Dump();
+						return null;
+				}
+			}
+			
+			return theValue;
+		}
+
+		static string GetDefaultValue(Type t)
+		{
+			if (Nullable.GetUnderlyingType(t) != null) return "null";
+
+			if (t == typeof(DateTime)) return "\"0001-01-01T00:00:00\"";
+			if (t == typeof(Guid)) return "\"00000000-0000-0000-0000-000000000000\"";
+
+			return null;
+		}
+
+		static string GetInt32Value(object value, Type returnType)
+		{
+			if (!(value is int)) return null;
+			
+			if (returnType == typeof(ulong) || returnType == typeof(ulong?))
+			{
+				return GetInt64Value((long)(int)value, returnType);
+			}
+			
+			var num = (int)value;
+			if (returnType == typeof(char) || returnType == typeof(char?))
+			{
+				return GetEscapedString(((char)num).ToString());
+			}
+			
+			if (returnType == typeof(bool) || returnType == typeof(bool?))
+			{
+				return num == 0 ? "false" : "true";
+			}
+			
+			if (returnType == typeof(uint) || returnType == typeof(uint?))
+			{
+				var unum = (uint)num;
+				return unum.ToString(System.Globalization.CultureInfo.InvariantCulture);
+			}
+			
+			return num.ToString(System.Globalization.CultureInfo.InvariantCulture);
+		}
+
+		static string GetInt64Value(object value, Type returnType)
+		{
+			if (!(value is long)) return null;
+			
+			var num = (long)value;	
+
+			if (returnType == typeof(ulong) || returnType == typeof(ulong?))
+			{
+				var unum = (ulong)num;
+				return unum.ToString(System.Globalization.CultureInfo.InvariantCulture);
+			}
+
+			return num.ToString(System.Globalization.CultureInfo.InvariantCulture);
+		}
+
+		static string GetEscapedString(string value)
+		{
+			// fixme escape
+			return "\"" + value + "\"";
+		}
+
+		static string GetDoubleValue(object value, Type returnType)
+		{
+			if (!(value is double))
+			{
+				if (value is float)
+				{
+					value = (double)(float)value;
+				}
+				else
+				{
+					return null;
+				}
+			}
+			
+			var num = (double)value;	
+			return num.ToString("R", System.Globalization.CultureInfo.InvariantCulture);
+		}
+	}
+}
