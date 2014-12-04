6138fd2ee8cee8fabe5261cf6eb5245355a867f4
Cameron Jordan <cjordan@cameronism.com>
2014-12-03 23:52:05 -0800

    Start of enum string lookup generator

---
+++ Cameronism.Json.Tests/StringEnumTest.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cameronism.Json.Tests
+{
+	public unsafe class StringEnumTest
+	{
+		[Fact]
+		public void DayOfWeekEnum()
+		{
+			var buffer = new byte[1024];
+			int stringStart;
+			int freeStart;
+
+			fixed (byte* ptr = buffer)
+			{
+				var lookup = StringEnum.GenerateSequentialLookup(typeof(DayOfWeek), ptr, buffer.Length, out freeStart);
+
+				Assert.True(lookup.HasValue, "lookup must have been generated");
+				stringStart = (int)(lookup.Value.StringStart - ptr);
+				
+				//var chars = new char[64];
+				//foreach (DayOfWeek dow in Enum.GetValues(typeof(DayOfWeek)))
+				//{
+				//	byte* str;
+				//	int len;
+				//	lookup.Value.FindSequential((ulong)dow, out str, out len);
+				//	Assert.True(str != null, "string must not be null");
+				//	var dowString = dow.ToString();
+				//	Assert.Equal(dowString.Length, len);
+
+				//	string actualString;
+				//	fixed (char* ch = chars)
+				//	{
+				//		var strLen = Encoding.UTF8.GetChars(str, len, ch, chars.Length);
+				//		actualString = new string(ch, 0, strLen);
+				//	}
+				//	Assert.Equal(dowString, actualString);
+				//}
+			}
+
+			var sb = new StringBuilder();
+			sb.AppendLine("## Lookups");
+			Util.Hex.Dump(sb, buffer.Take(stringStart));
+			sb.AppendLine();
+			sb.AppendLine("## Strings");
+			Util.Hex.Dump(sb, buffer.Skip(stringStart).Take(freeStart - stringStart));
+
+			ApprovalTests.Approvals.Verify(sb.ToString());
+		}
+	}
+}
---
+++ Cameronism.Json/StringEnum.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cameronism.Json
+{
+	unsafe internal class StringEnum
+	{
+		public struct Lookup
+		{
+			readonly byte* _TableStart;
+			readonly byte* _StringStart;
+
+			public byte* TableStart { get { return _TableStart; } }
+			public byte* StringStart { get { return _StringStart; } }
+
+			public Lookup(byte* tableStart, byte* stringStart)
+			{
+				_TableStart = tableStart;
+				_StringStart = stringStart;
+			}
+
+			public void FindSequential(ulong value, out byte* str, out int length)
+			{
+				byte* key = _TableStart + value;
+				if (key >= _StringStart)
+				{
+					str = null;
+					length = 0;
+					return;
+				}
+
+				str = _StringStart + *(ushort*)key;
+				length = *(ushort*)(key + 2);
+			}
+		}
+
+		public static Lookup? GenerateSequentialLookup(Type type, byte* destination, int available, out int used)
+		{
+			used = 0;
+
+			var maybeMaxValue = GetMaxValue(type);
+			if (!maybeMaxValue.HasValue) return null;
+			var maxValue = maybeMaxValue.Value + 1;
+
+			byte* endStrings = destination + available;
+			byte* stringPtr = (byte*)(destination + (maxValue * 4));
+			var stringsStart = stringPtr;
+
+			if (stringPtr >= destination + available) return null;
+
+			for (ulong i = 0; i < maxValue; i++)
+			{
+				long offset = stringPtr - stringsStart;
+				if (offset > ushort.MaxValue) return null;
+
+				string enumName = Enum.GetName(type, i);
+				if (enumName == null)
+				{
+					*(uint*)(destination + (i * 4)) = 0;
+					continue;
+				}
+
+				*(ushort*)(destination + (i * 4)) = (ushort)offset;
+
+				int result = ConvertUTF.WriteStringUtf8(enumName, stringPtr, (int)(endStrings - stringPtr));
+				if (result <= 0 || result >= ushort.MaxValue) return null;
+
+				*(ushort*)(destination + (i * 4) + 2) = (ushort)result;
+				stringPtr += result;
+			}
+
+			used = (int)(stringPtr - destination);
+			return new Lookup(destination, stringsStart);
+		}
+
+		public static ulong? GetMaxValue(Type type)
+		{
+			bool any = false;
+			ulong maxValue = 0;
+			foreach (var val in Enum.GetValues(type))
+			{
+				any = true;
+				var num = Convert.ToUInt64(val);
+				if (num > maxValue) maxValue = num;
+			}
+
+			return any ? maxValue : (ulong?)null;
+		}
+	}
+}
